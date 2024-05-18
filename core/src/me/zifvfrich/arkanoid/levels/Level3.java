84a8e3ba741f32000a69bca3f43bbc30b35c83f7
popkasmelya <zifvfrich@gmail.com>
2024-05-19 02:07:53 +0300

    fix level 3

--- core/src/me/zifvfrich/arkanoid/levels/Level3.java
+++ core/src/me/zifvfrich/arkanoid/levels/Level3.java
@@ -16,7 +16,7 @@ public class Level3 extends Level {
         bricks = new Array<>(false, 14*4);
         background = Assets.background2;
 
-        for (int y = GameScreen.boundY- Brick.height*6; y > GameScreen.boundY-Brick.height*14-1; y -= Brick.height*2) {
+        for (int y = GameScreen.boundY- Brick.height; y > GameScreen.boundY-Brick.height*14-1; y -= Brick.height*3) {
             for (int x = GameScreen.boundX1+1; x < GameScreen.boundX2; x += Brick.width) {
                 bricks.add(new Brick(Assets.bricks.random(), x, y));
             }
